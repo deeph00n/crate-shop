0440799eece73f1d3bd0f4018cbbc8d26efe92f6
Jan Steffens <jan-steffens@live.com>
2019-10-26 02:18:04 +0200

    added product list endpoint using orientdb as data storage

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,65 @@
 use actix_files as fs;
-use actix_web::{middleware, App, HttpServer};
+use actix_web::{web, App, HttpServer, middleware, HttpResponse};
+use orientdb_client::{OrientDB};
+use ron::de::from_reader;
+use serde::{Deserialize,Serialize};
+use std::{fs::File};
+
+
+#[derive(Debug, Serialize)]
+struct Product {
+    name: String,
+    price: i32,
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    db_host: String,
+    db_port: u16,
+    db_database: String,
+    db_user: String,
+    db_password: String,
+}
+
+fn read_config() -> Config {
+    let f = File::open("config.ron").expect("Failed opening config.ron");
+    let config: Config = from_reader(f).unwrap();
+    config
+}
+
+fn list_products() -> HttpResponse {
+    let config = read_config();
+    let client = OrientDB::connect((config.db_host,config.db_port)).unwrap();
+
+    let session = client.session(&config.db_database, &config.db_user,&config.db_password).unwrap();
+
+    let results = session
+        .query("select from Product")
+        .run()
+        .expect("Unable to query the class Product")
+        .map(|e| {
+            e.map(|i| Product {
+                name: i.get("Name"),
+                price: i.get("Price"),
+            })
+        })
+        .collect::<Result<Vec<Product>, _>>().unwrap();
+    HttpResponse::Ok().json(results)
+}
 
 fn main() -> std::io::Result<()> {
+
+
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
+
+
 
     HttpServer::new(|| {
         App::new()
             // enable logger
             .wrap(middleware::Logger::default())
+            .service(web::resource("/product").route(web::get().to(list_products)))
             .service(
                 // static files
                 fs::Files::new("/", "./../www/").index_file("index.html"),
