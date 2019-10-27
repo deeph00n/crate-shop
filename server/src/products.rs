28461a564230d1db5a69269937dceb4d62623bd7
Jan Steffens <jan-steffens@live.com>
2019-10-27 08:46:14 +0100

    prep to work with binary files from orient db

--- server/src/products.rs
+++ server/src/products.rs
@@ -6,18 +6,20 @@ use serde::{Serialize};
 #[derive(Debug, Serialize)]
 struct Product {
     name: String,
+    image: String,
     price: i32,
 }
 
 pub fn list(session: web::Data<OSession>) -> HttpResponse {
     let results = session
-        .query("select from Product")
+        .query("select Name,Price,Image.Name from Product")
         .run()
         .expect("Unable to query the class Product")
         .map(|e| {
             e.map(|i| Product {
                 name: i.get("Name"),
                 price: i.get("Price"),
+                image: i.get("Image.Name"),
             })
         })
         .collect::<Result<Vec<Product>, _>>().expect("Unable to collect result");
